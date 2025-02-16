d15782e6fb8782e52327dbd7fa38df51f200afb5
anikush-yes <anikush@hotmail.com>
2025-02-16 14:36:12 +0200

    modified ToDoListTests.cy.js in e2e
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/ToDoListTests.cy.js
+++ cypress/e2e/ToDoListTests.cy.js
@@ -1,28 +1,35 @@
-describe('First Test', () => {
-    it('Opens the website', () => {
-      cy.visit('https://todolist.james.am/#/');
-      cy.url().should('eq', 'https://todolist.james.am/#/');
-    });
+describe('Each test includes visiting the website', () => {
+  beforeEach(() => {
+    cy.visit('https://todolist.james.am/#/');
+
   });
 
 
   describe('Header Element Test', () => {
     it('Checks if <header> is visible on the page', () => {
-      cy.visit('https://todolist.james.am/#/'); 
       cy.get('header').should('be.visible');
     });
   });
 
+
   describe('Header Text Test', () => {
     it('Checks if <header> contains "To Do List"', () => {
-      cy.visit('https://todolist.james.am/#/'); 
       cy.get('header h1').should('be.visible').and('have.text', 'To Do List');
     });
   });
 
+
+  describe('Create new to do Test', () => {
+    it('Creates new to do', () => {
+      cy.get('input.new-todo').type('New to do created{enter}');
+      cy.contains('ul.todo-list li', 'New to do created').should('be.visible');
+    });
+
+  });
+
+
   describe('Footer Text Test', () => {
     it('Checks if <footer> contains the text "Double-click to edit a todo"', () => {
-      cy.visit('https://todolist.james.am/#/'); // Aplankome svetainę
       cy.get('footer.info p').should('exist');
       cy.get('footer.info p').should('be.visible').and('have.text', 'Double-click to edit a toodo'); // Patikriname tekstą. //gramatinė klaida"..toodo vietoj "todo"
     });
@@ -31,23 +38,57 @@ describe('First Test', () => {
 
   describe('Input Placeholder Test', () => {
     it('Checks if input has the placeholder "What needs to be done?"', () => {
-      cy.visit('https://todolist.james.am/#/'); 
-      cy.get('input.new-todo').should('have.attr', 'placeholder', "What need's to be done?"); 
+      cy.get('input.new-todo').should('have.attr', 'placeholder', "What need's to be done?");
     });
   });
 
-  describe('To-Do List Test', () => {
+
+  describe('Add multiple tasks To-Do List Test', () => {
     it('Adds multiple tasks and checks if the list is not empty', () => {
-      cy.visit('https://todolist.james.am/#/'); 
-  
-      
-      cy.get('input.new-todo').type('2 task{enter}');
+
+
+      cy.get('input.new-todo').type('1 task{enter}');
       cy.get('input.new-todo').type('2 task{enter}');
       cy.get('input.new-todo').type('3 task{enter}');
-  
-      
-      cy.get('ul.todo-list li') 
-        .should('exist') 
+
+
+      cy.get('ul.todo-list li')
+        .should('exist')
         .and('have.length.at.least', 3);
     });
   });
+
+
+  describe('Delete-To-Do List task Test', () => {
+    it('Delete new to do', () => {
+      cy.get('input.new-todo').type('1 task{enter}');
+      cy.get('input.new-todo').type('task to be deleted{enter}');
+
+      cy.contains('ul.todo-list li', 'task to be deleted').find('button.destroy').invoke('show');
+      cy.contains('ul.todo-list li', 'task to be deleted').find('button.destroy').click();
+
+      cy.contains('ul.todo-list li', 'task to  be deleted').should('not.exist');
+
+    });
+  });
+
+
+  describe('Edit To-Do List Item Test', () => {
+    it('To do item edit', () => {
+
+      cy.get('input.new-todo').type('1 task{enter}');
+      cy.get('input.new-todo').type('2 task{enter}');
+      cy.get('input.new-todo').type('3 task{enter}');
+
+      cy.contains('ul.todo-list li', '2 task')
+        .dblclick()
+        .find('input.edit')
+        .clear()
+        .type('edited task{enter}');
+
+      cy.contains('ul.todo-list li', 'edited task').should('be.visible');
+
+    });
+  });
+
+});
