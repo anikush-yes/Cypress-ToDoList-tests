9337459be5cca7359ee4633d0e20ab7be0f6a6f0
anikush-yes <anikush@hotmail.com>
2025-02-17 09:50:39 +0200

    added SessionsCookies.cy.js to e2e
    Signed-off-by: anikush-yes <anikush@hotmail.com>

---
+++ cypress/e2e/SessionsCookies.cy.js
@@ -0,0 +1,53 @@
+//elementu skaiciavimo susiskaiciuoti kiek mes turim li saraso elementu
+
+//Svetaine prisijungimas ir registracija
+//1.testuoti pacia registracija. Ar galima uzsiregistruoti?
+//2. pati prisijungima. Ar galima prisijungti?
+//3. Integracinis testas: Ar uzsiregistravus galima prisijungti?
+//4. musu rasomas testas tures buti prisijunges prie svetaines, atsiminti kad jis yra prisijunges
+//5. Kaip tas pats funkcionalumas yra atvaizduojamas prisijungusiam ir neprisijungusiam vartotojui
+
+//Prisijungimo atsiminmas(sesijos isaugojimas)
+//Sesija - serverio atminties vieta, kur svetainė gali išsaugoti informaciją
+//Cookie - vieta vartotojo kompiuteryje, kur svetainė gali išsaugoti informaciją
+
+//Kokią info saugau sesijoje? jautrūs duomenys - slaptažodis, prisijungimo vardas, prisijungimo tokenas, asmens informacija...
+// Kokią info saugau cookie? viską, išskyrūs jautrią informaciją
+
+//abiem galima nustatyti laiką, kiek jie egzistuoja
+
+describe('Check if website allows creating a cookie', () => {
+    beforeEach(() => {
+        cy.visit('https://todolist.james.am/#/');
+    });
+
+    it('Should allow creating and verifying a cookie', () => {
+        // Set a cookie
+        cy.setCookie('testCookie', 'cookieValue');
+
+        // Verify the cookie is set
+        cy.getCookie('testCookie').should('exist').and('have.property', 'value', 'cookieValue');
+    });
+
+    //Ar informacija issisaugo po svetaines perkrovimo?
+    //1. Uzeiti i svataine
+    //2. Sukurti informacini cookie (JSON masyva tekstiniame formate, kazkoks skaicius, tekstas ir t.t)
+    //3. Sukurti kelis cookies
+    //4. Perkrauti svataine
+    //5. Patikrinti ar cookie isliko po perkrovimo:
+
+
+    it('Ar informacija issisaugo po svetaines perkrovimo?', () => {
+        cy.setCookie('test', '1');
+        cy.setCookie('test1', 'test1');
+        cy.setCookie('test2', 'test1');
+
+        cy.reload();
+        cy.getCookie('test').should('exist');
+        cy.getCookie('test1').should('exist');
+        cy.getCookie('test2').should('exist');
+
+        // cy.clearCookie('test');
+        cy.clearAllCookies();
+    });
+});
